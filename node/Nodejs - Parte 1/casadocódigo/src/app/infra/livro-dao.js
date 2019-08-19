2cf8ddf29e2510dda67c55af758e202abb20db69
Guilherme <tocchettoo@gmail.com>
2019-08-19 14:06:53 -0300

    Curso node

--- node/Nodejs - Parte 1/casadocódigo/src/app/infra/livro-dao.js
+++ node/Nodejs - Parte 1/casadocódigo/src/app/infra/livro-dao.js
@@ -3,13 +3,43 @@ class LivroDao {
         this._db = db;
     }
 
-    lista(callback){
-        this._db.all(
+    adiciona(livro) {
+        return new Promise((resolve, reject) => {
+            this._db.run(`
+                INSERT INTO LIVROS (
+                        titulo,
+                        preco,
+                        descricao
+                    ) values (?, ?, ?)
+                `,
+                [
+                    livro.titulo,
+                    livro.preco,
+                    livro.descricao
+                ],
+                function (err) {
+                    if (err) {
+                        console.log(err);
+                        return reject('Não foi possível adicionar o livro!');
+                    }
+    
+                    resolve();
+                } 
+            )       
+        });
+    } 
+
+    lista(){
+        return new Promise((resolve, reject) => { this._db.all(
             'SELECT * FROM livros',
-            function(erro, resultados){
-                callback(erro, resultados);
+            (erro, resultados) => {
+                if (erro) return reject('Não foi possível listar os livros!');
+
+                return resolve(resultados);
             }
-        )
+            )});
+
+        
     }
 
 }
